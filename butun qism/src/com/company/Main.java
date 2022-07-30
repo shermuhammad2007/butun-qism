9905536389499fcb8bc826e30a431cb4cc38809b
shermuhammad2007 <110012651+shermuhammad2007@users.noreply.github.com>
2022-07-30 12:23:14 +0500

    Add files via upload

---
+++ butun qism/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 7; // berilgan son
+        int b = a / 3; // a ni 3ga bo'lib butun qismini b ga joylash.
+
+        System.out.println(b);
+    }
+}
